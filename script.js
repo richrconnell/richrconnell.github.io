4f6c6d02f0ccd49268956f0141462728572a6233
richrconnell <richryc@gmail.com>
2020-07-31 02:15:41 -0400

    Update for modal window

--- script.js
+++ script.js
@@ -193,3 +193,12 @@ function RecruitingSoftCalc() {
 
   console.log(salary);
 }
+var elements = $('.modal-overlay, .modal');
+
+$('button').click(function () {
+  elements.addClass('active');
+});
+
+$('.close-modal').click(function () {
+  elements.removeClass('active');
+});
